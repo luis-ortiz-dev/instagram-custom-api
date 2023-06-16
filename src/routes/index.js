ec2d2368213275a632c471dbf0afa4fe61a79b07
Luis Ortiz <luis.ortiz.dev@gmail.com>
2023-06-16 17:11:40 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,8 @@ router.get('/', (req, res) => {
 router.get('/test', async (req, res) => {
     if (req.query.code)
     {
-        console.log("Code" + req.query.code);
+        console.log("Code: " + req.query.code);
+
         var clientId = '1171495670260486';
         var clientSecret = '118832bac0a74de45c2e4002a4aa66bb';
 
@@ -17,7 +18,7 @@ router.get('/test', async (req, res) => {
             'client_secret': clientSecret,
             'code': req.query.code,
             'grant_type': 'authorization_code',
-            'redirect_uri': 'https://www.lemonlabs.mx/test'
+            'redirect_uri': 'https://instagram-custom-api-b12eacc92c32.herokuapp.com/test'
         };
 
         var formBody = [];
@@ -28,6 +29,7 @@ router.get('/test', async (req, res) => {
         }
         formBody = formBody.join("&");
 
+        console.log("Begin short token ws");
         //SHORT TOKEN
         var shortTokenResponse = await fetch('https://api.instagram.com/oauth/access_token',
             {
@@ -40,6 +42,7 @@ router.get('/test', async (req, res) => {
         var jsonAccessToken = await shortTokenResponse.json();
         console.log(jsonAccessToken);
         
+        console.log("Begin long token ws");
         //LONG TOKEN
         var longTokenResponse = await fetch(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${clientSecret}&access_token=${jsonAccessToken.access_token}`, {
             method: 'GET'
@@ -48,6 +51,7 @@ router.get('/test', async (req, res) => {
         var longTokenJson = await longTokenResponse.json();
         console.log(longTokenJson);
 
+        console.log("Begin user profile ws");
         // USER PROFILE
         var userProfileResponse = await fetch(`https://graph.instagram.com/${jsonAccessToken.user_id}?fields=id,username&access_token=${longTokenJson.access_token}`, {
             method: 'GET'
